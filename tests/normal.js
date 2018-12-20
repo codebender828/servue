e87123978cce6937d8b20fbe351ca527b9afbfac
DominusVilicus <albertmashy@outlook.com>
2018-12-20 15:52:19 +1030

    added normal to test without jest

---
+++ tests/normal.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment node
+ */
+const path = require("path")
+const axios = require("axios")
+const Renderer = require("../lib")
+var renderer = new Renderer()
+renderer.resources = path.resolve(__dirname, "resources")
+renderer.nodemodules = path.resolve(__dirname,'../node_modules')
+
+console.log(render())
+
+async function render(){
+    var response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    var html = await renderer.render("home", {
+        info: response.data,
+        msg: "Lala!",
+        messageOuter: "Nla Nla"
+    })
+    return html
+}
